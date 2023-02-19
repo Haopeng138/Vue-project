b3c064f53425e3eb63053534c49c22e9f444ba14
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2023-02-19 01:51:14 +0100

    feat(custom): new css for login page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="box"></div>
   <router-view/>
 </template>
 
 <script setup>
-const boxWidht = '100px'
+
 </script>
 
 <style lang="scss">
-.box{
-  width: v-bind(boxWidht);
-  height: 100px;
-  background-color: red;
-}
+
 </style>
